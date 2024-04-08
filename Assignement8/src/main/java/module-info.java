66a106c72681c18b9875c760782c72587ba6a4d8
courcellela <166452055+courcellela@users.noreply.github.com>
2024-04-08 15:41:11 -0700

    Delete Assignement8 directory

--- Assignement8/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.assignements.assignement8 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens org.assignements.assignement8 to javafx.fxml;
-    exports org.assignements.assignement8;
-}
